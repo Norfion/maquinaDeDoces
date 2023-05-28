7890e9cbd28fa5e55f4359e0c62b69c0549ff246
Normando Nascimento <99773886+Norfion@users.noreply.github.com>
2023-05-27 21:45:02 -0300

    Dividindo modos e Loops
    - Dividi os modos de Administrador e Usuário em duas faixas de opções
    - Crie um loop para que a máquina fique sempre ativa

--- main.cpp
+++ main.cpp
@@ -12,8 +12,6 @@ struct produto {
 int main() {
   struct produto produtos[qtdProdutos];
 
-  bool liberacaoDoProduto = false;
-
   // Atribui informações aos produtos
   produtos[0].nome = "Banana";
   produtos[0].quantidade = 5;
@@ -25,47 +23,63 @@ int main() {
     produtos[i].codigo = (i + 1);
   }
 
-  // Exibe os produtos da máquina
-  cout << "------MÁQUINA------" << endl;
-  cout << "Código | Produto | Quantidade" << endl;
-  for (int i = 0; i < qtdProdutos; i++) {
-    cout << produtos[i].codigo << " - " << produtos[i].nome << " ("
-         << produtos[i].quantidade << ")" << endl;
-  }
-  cout << "\nInsira o código do produto desejado: ";
+  while (true) {
+    string opcaoDeModo;
 
-  int codigoSelecionado;
-  cin >> codigoSelecionado;
+    cout << "Digite algo para continuar: ";
+    cin >> opcaoDeModo;
 
-  // Verifica qual código o usuario selecionou e exibe o produto
-  for (int i = 0; i < qtdProdutos; i++) {
-    if (codigoSelecionado == produtos[i].codigo) {
-      cout << produtos[i].nome << " R$" << produtos[i].preco << endl;
-    }
-  }
-  // Confirma se o usuario deseja continuar
-  string opcaoSelecionada;
-  cout << "Confirmar a compra? ";
-  cin >> opcaoSelecionada;
+    if (opcaoDeModo == "admads2023") {
 
-  // Recebe o valor e verifica se o usuario tem dinheiro suficiente
-  float valorInserido;
-  if (opcaoSelecionada == "S" || opcaoSelecionada == "s") {
-    cout << "Insira o valor de depósito: R$";
-    cin >> valorInserido;
-    if (valorInserido < produtos[codigoSelecionado - 1].preco) {
-      cout << "Dinheiro insuficiente!";
-    } else if (valorInserido == produtos[codigoSelecionado - 1].preco) {
-      cout << "Obrigado pela compra!" << endl;
-      cout << "(Sem troco)" << endl;
-      liberacaoDoProduto = true;
-      produtos[codigoSelecionado - 1].quantidade--;
-    } else if (valorInserido > produtos[codigoSelecionado - 1].preco) {
-      cout << "Obrigado pela compra!" << endl;
-      cout << "Troco R$"
-           << valorInserido - produtos[codigoSelecionado - 1].preco;
-      produtos[codigoSelecionado - 1].quantidade--;
-      liberacaoDoProduto = true;
+    } else {
+      while (true) {
+        bool liberacaoDoProduto = false;
+        // Exibe os produtos da máquina
+        cout << "------MÁQUINA------" << endl;
+        cout << "Código | Produto | Quantidade" << endl;
+        for (int i = 0; i < qtdProdutos; i++) {
+          cout << produtos[i].codigo << " - " << produtos[i].nome << " ("
+               << produtos[i].quantidade << ")" << endl;
+        }
+        cout << "\nInsira o código do produto desejado: ";
+        int codigoSelecionado;
+        cin >> codigoSelecionado;
+        if (codigoSelecionado == 0) {
+          break;
+        }
+
+        // Verifica qual código o usuario selecionou e exibe o produto
+        for (int i = 0; i < qtdProdutos; i++) {
+          if (codigoSelecionado == produtos[i].codigo) {
+            cout << produtos[i].nome << " R$" << produtos[i].preco << endl;
+          }
+        }
+        // Confirma se o usuario deseja continuar
+        string opcaoSelecionada;
+        cout << "Confirmar a compra? ";
+        cin >> opcaoSelecionada;
+
+        // Recebe o valor e verifica se o usuario tem dinheiro suficiente
+        float valorInserido;
+        if (opcaoSelecionada == "S" || opcaoSelecionada == "s") {
+          cout << "Insira o valor de depósito: R$";
+          cin >> valorInserido;
+          if (valorInserido < produtos[codigoSelecionado - 1].preco) {
+            cout << "Dinheiro insuficiente!";
+          } else if (valorInserido == produtos[codigoSelecionado - 1].preco) {
+            cout << "Obrigado pela compra!" << endl;
+            cout << "(Sem troco)" << endl;
+            liberacaoDoProduto = true;
+            produtos[codigoSelecionado - 1].quantidade--;
+          } else if (valorInserido > produtos[codigoSelecionado - 1].preco) {
+            cout << "Obrigado pela compra!" << endl;
+            cout << "Troco R$"
+                 << valorInserido - produtos[codigoSelecionado - 1].preco;
+            produtos[codigoSelecionado - 1].quantidade--;
+            liberacaoDoProduto = true;
+          }
+        }
+      }
     }
   }
 }
